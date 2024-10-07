ccf23dc1ed12d2caf724ba504e253283bfb7a489
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-10-07 11:15:39 +0200

    added tiny class to encapsulate loading dataframe from gsheets

---
+++ src/memobuch_preprocessing/GSheet.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+class GSheet:
+    """
+    Class to handle Google Sheets
+    """
+
+    @staticmethod
+    def request_public_sheet(sheet_id: str, sheet_name: str) -> pd.DataFrame:
+        """
+        Get a public Google Sheet
+        """
+        url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+        return pd.read_csv(url)

23be45aac8a2e5029d1e1c51ebd1fd1989cb94fa
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-10-07 11:34:01 +0200

    added person number to memo_event

--- src/memobuch_preprocessing/MemoEvent.py
+++ src/memobuch_preprocessing/MemoEvent.py
@@ -5,9 +5,10 @@ class MemoEvent:
     """
     Represents an event for the memo project.
     """
-    def __init__(self, id: str, title: str, type: str, description: str, start_date: str, end_date: str, category: str, location: str, latt: float, long: float):
+    def __init__(self, id: str, title: str, person_number: str, type: str, description: str, start_date: str, end_date: str, category: str, location: str, latt: float, long: float):
         self.id = id
         self.title = title
+        self.person_number = person_number
         self.type = type
         self.description = description
         self.start_date = start_date

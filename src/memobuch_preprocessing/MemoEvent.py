21ae92e6f0b2f8bf73db7abd444ce1eb7b8671ee
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-10-16 09:35:35 +0200

    corrected naming of variables in class
    https://github.com/memobuch/preprocessing/issues/2

--- src/memobuch_preprocessing/MemoEvent.py
+++ src/memobuch_preprocessing/MemoEvent.py
@@ -3,10 +3,10 @@ class MemoEvent:
     """
     Represents an event for the memo project.
     """
-    def __init__(self, id: str, title: str, person_number: str, type: str, description: str, start_date: str, end_date: str, category: str, location: str, latt: float, long: float):
+    def __init__(self, id: str, title: str, person_ids: list[str], type: str, description: str, start_date: str, end_date: str, category: str, location: str, latt: float, long: float):
         self.id = id
         self.title = title
-        self.person_number = person_number
+        self.person_ids = person_ids
         self.type = type
         self.description = description
         self.start_date = start_date
@@ -18,4 +18,4 @@ class MemoEvent:
 
 
     def __repr__(self) -> str:
-        return f"MemoEvent({self.id}, {self.title}, {self.person_number}, {self.type}, {self.description}, {self.start_date}, {self.end_date}, {self.category}, {self.location}, {self.latt}, {self.long})"
+        return f"MemoEvent({self.id}, {self.title}, {self.person_ids}, {self.type}, {self.description}, {self.start_date}, {self.end_date}, {self.category}, {self.location}, {self.latt}, {self.long})"

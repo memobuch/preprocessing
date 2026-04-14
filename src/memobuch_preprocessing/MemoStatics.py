734e461e1ff9f9c9ebe0557d692bda893bd695f6
Sebastian Schiller-Stoff <sebi.stoff@gmail.com>
2026-04-14 10:28:06 +0200

    added basic variables to MemoStatics
    https://zimlab.uni-graz.at/gams5/projects/memo/memo-preprocessing/-/work_items/1

--- src/memobuch_preprocessing/MemoStatics.py
+++ src/memobuch_preprocessing/MemoStatics.py
@@ -8,3 +8,6 @@ class MemoStatics:
     OUTPUT_DIR = "output"
     PROJECT_ABBR = "memo"
     MATERIAL_ROOT_PATH = 'C:\\Users\\stoffse\\Meine Ablage\\01_Berufliches\\01_Projekte\\10_memo\\02_data\\material\\'
+
+    MEMO_BASE_URI = "https://www.ns-opfer-graz.at/"
+    MEMO_ONTOLOGY = MEMO_BASE_URI + "ontology#"

001cacb3733c55f83a4bf356687d4293e65eb175
Sebi <SebastianStoff@gmx.at>
2026-04-20 13:03:55 +0200

    changed projectAbbr to memor
    https://zimlab.uni-graz.at/gams5/projects/memo/memo-preprocessing/-/work_items/1

--- src/memobuch_preprocessing/MemoStatics.py
+++ src/memobuch_preprocessing/MemoStatics.py
@@ -6,7 +6,7 @@ class MemoStatics:
     """
 
     OUTPUT_DIR = "output"
-    PROJECT_ABBR = "memo"
+    PROJECT_ABBR = "memor"
     MATERIAL_ROOT_PATH = 'C:\\Users\\stoffse\\Meine Ablage\\01_Berufliches\\01_Projekte\\10_memo\\02_data\\material\\'
 
     MEMO_BASE_URI = "https://www.ns-opfer-graz.at/"

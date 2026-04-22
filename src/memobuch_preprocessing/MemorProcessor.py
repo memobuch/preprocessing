adda10c2a63debea051ca6450ec1bb2183e7ad1e
Sebi <SebastianStoff@gmx.at>
2026-04-22 08:19:55 +0200

    renamed to MemorPerson
    https://zimlab.uni-graz.at/gams5/projects/memo/memo-preprocessing/-/work_items/1

--- src/memobuch_preprocessing/MemorProcessor.py
+++ src/memobuch_preprocessing/MemorProcessor.py
@@ -11,7 +11,7 @@ import pandas as pd
 from memobuch_preprocessing.GSheet import GSheet
 from memobuch_preprocessing.MemorEvent import MemoEvent
 from memobuch_preprocessing.MemorVocab import MemoVocab
-from memobuch_preprocessing.Person.MemoPerson import MemoPerson
+from memobuch_preprocessing.Person.MemorPerson import MemoPerson
 from memobuch_preprocessing.MemorStatics import MemoStatics
 from memobuch_preprocessing.Person.MemoPersonFile import MemoPersonFile
 from memobuch_preprocessing.geo.FeatureAggregator import FeatureAggregator

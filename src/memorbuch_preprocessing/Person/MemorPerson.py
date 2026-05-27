3ca82bd595eef8d418f3079a3d3b3ea3a9e29944
Sebi <SebastianStoff@gmx.at>
2026-05-27 13:32:49 +0200

    added letzte freiwillige wohnadresse and DERLA sign to the data
    https://zimlab.uni-graz.at/gams5/projects/memo/memo-preprocessing/-/work_items/4

--- src/memorbuch_preprocessing/Person/MemorPerson.py
+++ src/memorbuch_preprocessing/Person/MemorPerson.py
@@ -119,6 +119,14 @@ class MemorPerson:
 
         description_element = ET.SubElement(root, 'dc:description', {'xml:lang': 'de'})
         description_element.text = self.biography_text
+        # add voluntary address to description element
+        description_element.text += f" Letzte freiwillige Wohnadresse: {self.voluntary_address if len(self.voluntary_address) > 1 else '-' }"
+        # add linked DERLA memorial signs if available
+        if len(self.memorial_signs) > 0:
+            description_element.text += " | Verbundene DERLA-Zeichen: "
+            for sign in self.memorial_signs:
+                description_element.text += f"{sign};"
+
 
         date_element = ET.SubElement(root, 'dc:date')
         date_element.text = "2026"
